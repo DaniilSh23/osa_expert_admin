7f9f435ca7e401382f415987afdc47d242268ab6
DaniilSh23 <danyasevas11@gmail.com>
2023-09-09 15:25:11 +0300

    изменил entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,18 +12,18 @@ python manage.py collectstatic --noinput
 echo "Apply database migrations"
 python manage.py migrate
 
+## Set keys in Django project
+#echo "Set keys in Django project"
+python manage.py setkeys
+
+## Load DB data from fixture
+#echo "Load DB data from fixture"
+python manage.py loaddata *fixture.json
+
 # Create superuser for Django
 echo "Create superuser for Django"
 echo "Username '${DJANGO_SUPERUSER_USERNAME}' | Password '${DJANGO_SUPERUSER_PASSWORD}' | mail '${DJANGO_SUPERUSER_EMAIL}'"
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='${DJANGO_SUPERUSER_USERNAME}').exists() or User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', '${DJANGO_SUPERUSER_EMAIL}', '${DJANGO_SUPERUSER_PASSWORD}')" | python manage.py shell
-
-## Set keys in Django project
-#echo "Set keys in Django project"
-#python manage.py setkeys
-
-## Load DB data from fixture
-#echo "Load DB data from fixture"
-#python manage.py loaddata db_data_*
 
 ## Download nltk dependencies
 echo "Download nltk dependencies"
